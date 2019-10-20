2eaa409a8ecdfaf94772e45516b90f865e5ad1be
ahmadooof <aa224rm@student.lnu.se>
2019-10-20 16:38:37 +0200

    adding pair functionality on click same of two pictures.

--- src/js/memoryGame.js
+++ src/js/memoryGame.js
@@ -8,19 +8,43 @@ export default class memoryGame {
   }
 
   createImages (rows, columns, template, container) {
-
+    let turn1
+    let turn2
+    let lastTile
     this.tiles.forEach(function (tile, index) {
       const a = document.importNode(template, true)
       container.appendChild(a)
-
       a.addEventListener('click', function (event) {
-        turnTile(tile, index, event.target)
+        let img = event.target.nodeName === 'IMG' ? event.target : event.target.firstElementChild
+        turnTile(tile, index, img)
       })
 
-      function turnTile (tile, index, element) {
-        const img = element.nodeName === 'IMG' ? element : element.firstElementChild
+      function turnTile (tile, index, img) {
 
+        console.log(tile)
+        console.log(index)
+        console.log(img)
+        if(turn2){
+          return
+        }
         img.src = 'image/memoryGame/' + tile + '.png'
+
+
+        if (!turn1) {
+          turn1 = img
+          lastTile = tile
+          return
+        } else {
+          if(img === turn1){
+            return;
+          }
+          turn2 = img;
+          if (tile === lastTile) {
+            console.log('pair')
+          }
+          turn1 = null
+          turn2 = null
+        }
       }
 
       // creating new line after each row (4 images)
