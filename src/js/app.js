2a0edc53c5412b02d104961f90eb7406f9011a4a
ahmadooof <aa224rm@student.lnu.se>
2019-10-21 15:21:29 +0200

    adding move window feature

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,41 @@
 import MemoryGame from './memoryGame.js'
-import windowObject from './window.js'
 
 const memoryGame = new MemoryGame(4, 4, 'memoryContainer')
 
-const window = document.querySelector('#window')
-const memoryGameButton = document.querySelector("#memory")
+const window = document.querySelector('.window')
+const memoryGameButton = document.querySelector('#memory')
+const navWindow = document.querySelector('.navWindow')
 
-
-memoryGame.addEventListener('click', function () {
-
+memoryGameButton.addEventListener('click', function () {
+  window.style.display = 'block'
 })
+
+let mousePosition
+let coordinates = [0, 0]
+let isDown = false
+
+navWindow.addEventListener('mousedown', function (event) {
+  isDown = true
+  coordinates = [
+    navWindow.offsetLeft - event.clientX - 100,
+    navWindow.offsetTop - event.clientY - 10
+  ]
+}, true)
+
+navWindow.addEventListener('mouseup', function () {
+  isDown = false
+}, true)
+
+navWindow.addEventListener('mousemove', function (event) {
+  if (isDown) {
+    console.log(event.clientX)
+    mousePosition = {
+      x: event.clientX,
+      y: event.clientY
+    }
+    window.style.marginLeft = (mousePosition.x + coordinates[0]) + 'px'
+    window.style.marginTop = (mousePosition.y + coordinates[1]) + 'px'
+  }
+
+}, true)
+
