8cc267c447fde957a07d87174add84c5c80ac8a3
ahmadooof <aa224rm@student.lnu.se>
2019-10-21 19:19:47 +0200

    creating 1 window with game inside it still have bug for many windows

--- src/js/app.js
+++ src/js/app.js
@@ -2,40 +2,46 @@ import MemoryGame from './memoryGame.js'
 import window from './window.js'
 
 const memoryGameButton = document.querySelector('#memory')
+let navWindow
+let windowDiv
 
 memoryGameButton.addEventListener('click', function () {
-  const memoryGame = new MemoryGame(4, 4, 'memoryContainer')
-  const myWindow = new window('.windowContainer')
-  myWindow.container.appendChild(memoryGame.container)
-
+  let memoryGame = new MemoryGame(4, 4, 'memoryContainer')
+  let myWindow = new window('.windowContainer')
+  myWindow.div.appendChild(memoryGame.container)
+  navWindow = document.querySelector('.navWindow')
+  windowDiv = document.querySelector('.window')
+  moveWindow(windowDiv)
 })
 
 let mousePosition
 let coordinates = [0, 0]
 let isDown = false
 
-// navWindow.addEventListener('mousedown', function (event) {
-//   isDown = true
-//   coordinates = [
-//     navWindow.offsetLeft - event.clientX - 100,
-//     navWindow.offsetTop - event.clientY - 10
-//   ]
-// }, true)
-//
-// navWindow.addEventListener('mouseup', function () {
-//   isDown = false
-// }, true)
-//
-// navWindow.addEventListener('mousemove', function (event) {
-//   if (isDown) {
-//     console.log(event.clientX)
-//     mousePosition = {
-//       x: event.clientX,
-//       y: event.clientY
-//     }
-//     window.style.marginLeft = (mousePosition.x + coordinates[0]) + 'px'
-//     window.style.marginTop = (mousePosition.y + coordinates[1]) + 'px'
-//   }
-//
-// }, true)
+function moveWindow (windowDiv) {
+  navWindow.addEventListener('mousedown', function (event) {
+    isDown = true
+    coordinates = [
+      navWindow.offsetLeft - event.clientX - 100,
+      navWindow.offsetTop - event.clientY - 10
+    ]
+  }, true)
 
+  navWindow.addEventListener('mouseup', function () {
+    isDown = false
+  }, true)
+
+  navWindow.addEventListener('mousemove', function (event) {
+    if (isDown) {
+      console.log(event.clientX)
+      mousePosition = {
+        x: event.clientX,
+        y: event.clientY
+      }
+      windowDiv.style.marginLeft = (mousePosition.x + coordinates[0]) + 'px'
+      windowDiv.style.marginTop = (mousePosition.y + coordinates[1]) + 'px'
+      windowDiv.style.marginTop = (mousePosition.y + coordinates[1]) + 'px'
+    }
+  }, true)
+
+}
--- src/js/window.js
+++ src/js/window.js
@@ -2,10 +2,9 @@ export default class window {
 
   constructor (container) {
     this.container = document.querySelector(container)
-    console.log(this.container)
     this.windowDiv = document.querySelectorAll('.windowContainer template')[0].content.firstElementChild
-    this.div = document.importNode(this.windowDiv, false)
-    console.log(this.div)
+    this.div = document.importNode(this.windowDiv, true)
     this.container.appendChild(this.div)
   }
+
 }
