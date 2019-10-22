560bedd539296c585a0ac4cfbb654b6b62c3469b
ahmadooof <aa224rm@student.lnu.se>
2019-10-22 19:22:43 +0200

    adding chat object to window object

--- src/js/chat.js
+++ src/js/chat.js
@@ -3,7 +3,8 @@ export default class chat {
   constructor (container) {
     this.container = document.querySelector(container)
     this.chatDiv = document.querySelectorAll('.chatContainer template')[0].content.firstElementChild
-    this.div = document.importNode(this.windowDiv, true)
+    this.div = document.importNode(this.chatDiv, true)
+    console.log(this.div)
     this.container.appendChild(this.div)
   }
 }
