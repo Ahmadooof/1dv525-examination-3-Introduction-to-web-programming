1f16593ebef9fcba7dfd029499f1fdfe6379cfb1
ahmadooof <aa224rm@student.lnu.se>
2019-10-22 22:37:35 +0200

    adding functionality to change username if exists in local storage

--- src/js/chat.js
+++ src/js/chat.js
@@ -6,13 +6,33 @@ export default class chat {
     this.div = document.importNode(this.chatDiv, true)
     this.container.appendChild(this.div)
 
+    /**
+     * if true => adding "Change UserName" button, adding textbox to set new username
+     * adding ok to take this name and store it in local storage.
+     */
     if(this.isUserNameExist())
     {
-      console.log(window.div.firstElementChild)
       let changeNameButton = document.createElement('button')
       changeNameButton.innerText = "Change Username"
       changeNameButton.className = 'changeNameButton'
       window.div.firstElementChild.appendChild(changeNameButton)
+      changeNameButton.addEventListener('click', (event) => {
+        changeNameButton.remove()
+        let textBox = document.createElement('input')
+        textBox.placeholder = "Enter UserName"
+        textBox.className = "UserNameTextBox"
+        window.div.firstElementChild.appendChild(textBox)
+        let buttonOk = document.createElement('button')
+        buttonOk.innerText = "OK"
+        buttonOk.className = "buttonOk"
+        window.div.firstElementChild.appendChild(buttonOk)
+        buttonOk.addEventListener('click', () => {
+          let nameValue = document.querySelector('.UserNameTextBox').value
+          localStorage.setItem('username', nameValue)
+          textBox.remove()
+          buttonOk.remove()
+        })
+    })
     }
   }
 
