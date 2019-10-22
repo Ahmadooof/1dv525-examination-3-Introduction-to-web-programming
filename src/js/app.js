a30e0d8e48ad9473abd35fafedda0ef73c61b38a
ahmadooof <aa224rm@student.lnu.se>
2019-10-22 15:13:27 +0200

    changing implementation of eventlistener to make it for each object window.. finally works

--- src/js/app.js
+++ src/js/app.js
@@ -5,52 +5,12 @@ const memoryGameButton = document.querySelector('#memory')
 
 let navWindow
 let windowDiv
-let countWindows = 0
 
+let test = document.querySelectorAll('.windowContainer template')[0].content.firstElementChild
+let nav = test.firstElementChild
 
 memoryGameButton.addEventListener('click', function () {
   let memoryGame = new MemoryGame(4, 4, 'memoryContainer')
   let myWindow = new window('.windowContainer')
   myWindow.div.appendChild(memoryGame.div)
-  navWindow = document.querySelectorAll('.navWindow')[countWindows]
-  windowDiv = document.querySelectorAll('.window')[countWindows]
-  countWindows++
 })
-let mousePosition
-let coordinates = [0, 0]
-let isDown = false
-document.body.addEventListener('mousedown', event => {
-  if (event.target !== navWindow) {
-    return
-  }
-  console.log(navWindow.offsetLeft)
-  isDown = true
-  coordinates = [
-    windowDiv.offsetLeft - event.clientX - 150 ,
-    windowDiv.offsetTop - event.clientY - 40
-  ]
-  console.log(coordinates[0])
-}, true)
-
-document.body.addEventListener('mouseup', event => {
-  if (event.target !== navWindow) {
-    return
-  }
-  mousePosition = {}
-  coordinates = [0, 0]
-  isDown = false
-}, true)
-
-document.body.addEventListener('mousemove', event => {
-  if (event.target !== navWindow) {
-    return
-  }
-  if (isDown) {
-    mousePosition = {
-      x : event.clientX,
-      y : event.clientY
-    }
-    windowDiv.style.left = (mousePosition.x + coordinates[0]) + 'px';
-    windowDiv.style.top  = (mousePosition.y + coordinates[1]) + 'px';
-  }
-}, true)
--- src/js/window.js
+++ src/js/window.js
@@ -1,39 +1,41 @@
 export default class window {
 
+  isDown
+  coordinates
+  mousePosition
+
   constructor (container) {
     this.container = document.querySelector(container)
     this.windowDiv = document.querySelectorAll('.windowContainer template')[0].content.firstElementChild
     this.div = document.importNode(this.windowDiv, true)
     this.container.appendChild(this.div)
 
-    let mousePosition
-    let coordinates = [0, 0]
-    let isDown = false
 
-    this.windowDiv.firstElementChild.addEventListener('mousedown', event => {
-      isDown = true
-      console.log(event.target)
-      coordinates = [
-        this.windowDiv.firstElementChild.offsetLeft - event.clientX - 100,
-        this.windowDiv.firstElementChild.offsetTop - event.clientY - 10
-      ]
-    }, true)
+    this.div.firstElementChild.addEventListener('mousedown', this.mouseDown, true)
+    this.div.firstElementChild.addEventListener('mouseup', this.mouseUp, true)
+    this.div.firstElementChild.addEventListener('mousemove', this.mouseMove, true)
+  }
 
-    this.windowDiv.firstElementChild.addEventListener('mouseup', function () {
-      isDown = false
-    }, true)
+  mouseDown (event) {
+    this.isDown = true
+    this.coordinates = [
+      this.parentNode.offsetLeft - event.clientX - 150,
+      this.parentNode.offsetTop - event.clientY - 40
+    ]
+  }
 
-    this.windowDiv.firstElementChild.addEventListener('mousemove', function (event) {
-      event.preventDefault()
-      if (isDown) {
-        console.log(event.clientX)
-        mousePosition = {
-          x: event.clientX,
-          y: event.clientY
-        }
-        this.windowDiv.style.marginLeft = (mousePosition.x + coordinates[0]) + 'px'
-        this.windowDiv.style.marginTop = (mousePosition.y + coordinates[1]) + 'px'
+  mouseUp () {
+    this.isDown = false
+  }
+
+  mouseMove (event) {
+    if (this.isDown) {
+      this.mousePosition = {
+        x: event.clientX,
+        y: event.clientY
       }
-    }, true)
+      this.parentNode.style.left = (this.mousePosition.x + this.coordinates[0]) + 'px'
+      this.parentNode.style.top = (this.mousePosition.y + this.coordinates[1]) + 'px'
+    }
   }
 }
