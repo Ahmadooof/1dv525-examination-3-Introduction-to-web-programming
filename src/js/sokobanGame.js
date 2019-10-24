7bc93e4f36776e835ecee521c4973f451f12932e
ahmadooof <aa224rm@student.lnu.se>
2019-10-24 08:19:30 +0200

    printing sokoban map adding some css to it

--- src/js/sokobanGame.js
+++ src/js/sokobanGame.js
@@ -1,13 +1,59 @@
 export default class sokobanGame {
 
-  constructor (container,window) {
-    // this.container = document.getElementById(container)
-    // this.memoryDiv = document.querySelectorAll('#memoryContainer template')[0].content.firstElementChild //this is memory div
-    // this.div = document.importNode(this.memoryDiv, false)
-    // this.tiles = this.shuffleArray(rows, columns)
-    // this.createImages(rows, columns, this.memoryDiv, this.div)
-    // this.container.appendChild(this.div)
+  constructor (container, window) {
+    this.tileMap = {
+      column: 19,
+      row: 16,
+      mapGrid: [
+        [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], [' '], [' '], ['W'], ['B'], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], ['W'], ['W'], ['W'], [' '], [' '], ['B'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], ['W'], [' '], [' '], ['B'], [' '], ['B'], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [['W'], ['W'], ['W'], [' '], ['W'], [' '], ['W'], ['W'], [' '], ['W'], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W']],
+        [['W'], [' '], [' '], [' '], ['W'], [' '], ['W'], ['W'], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], ['G'], ['G'], ['W']],
+        [['W'], [' '], ['B'], [' '], [' '], ['B'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], ['G'], ['G'], ['W']],
+        [['W'], ['W'], ['W'], ['W'], ['W'], [' '], ['W'], ['W'], ['W'], [' '], ['W'], ['P'], ['W'], ['W'], [' '], [' '], ['G'], ['G'], ['W']],
+        [[' '], [' '], [' '], [' '], ['W'], [' '], [' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W']],
+        [[' '], [' '], [' '], [' '], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], ['W'], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']],
+        [[' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' '], [' ']]
+      ]
+    }
+    this.container = document.querySelector(container)
+    this.gameDiv = document.querySelectorAll('.skoboanContainer template')[0].content.firstElementChild //this is memory div
 
-    console.log("hi")
+    // (0 = box) , (1 = goal), (2 = ground), (3 = person), (4 = wall)
+    this.allImages = this.gameDiv.querySelectorAll('img')
+    this.game = document.importNode(this.gameDiv, false)
+    this.printMap(this.allImages, this.tileMap)
+
+    this.container.appendChild(this.game)
+
+  }
+
+  printMap (allImages, tileMap) {
+    for (let row = 0; row < tileMap.row; row++) {
+      for (let column = 0; column < tileMap.column; column++) {
+        if (tileMap.mapGrid[row][column][0] === ' ') {
+          this.game.appendChild(document.importNode(this.allImages[2], false))
+        }
+        if (tileMap.mapGrid[row][column][0] === 'W') {
+          this.game.appendChild(document.importNode(this.allImages[4], false))
+        }
+        if (tileMap.mapGrid[row][column][0] === 'B') {
+          this.game.appendChild(document.importNode(this.allImages[0], false))
+        }
+        if (tileMap.mapGrid[row][column][0] === 'G') {
+          this.game.appendChild(document.importNode(this.allImages[1], false))
+        }
+        if (tileMap.mapGrid[row][column][0] === 'P') {
+          this.game.appendChild(document.importNode(this.allImages[3], false))
+        }
+      }
+    }
   }
 }
